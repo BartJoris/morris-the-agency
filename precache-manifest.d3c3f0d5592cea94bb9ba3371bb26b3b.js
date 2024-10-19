8f85f19bd3b72faaf40591f47713985a5dfd8b3a
Bart Joris <mail@bartjoris.be>
2024-10-19 23:45:23 +0200

    Updates

---
+++ precache-manifest.d3c3f0d5592cea94bb9ba3371bb26b3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06287e3733f15a072a7a6dfacb84843",
+    "url": "%3Cyour-github-username%3E.github.io/%3Cyour-repo-name%3E/index.html"
+  },
+  {
+    "revision": "3e58386cdafce626b049",
+    "url": "%3Cyour-github-username%3E.github.io/%3Cyour-repo-name%3E/static/css/main.951a629c.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "%3Cyour-github-username%3E.github.io/%3Cyour-repo-name%3E/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "3e58386cdafce626b049",
+    "url": "%3Cyour-github-username%3E.github.io/%3Cyour-repo-name%3E/static/js/main.36dc94d9.chunk.js"
+  },
+  {
+    "revision": "d4472e5a60b0a42ecf27",
+    "url": "%3Cyour-github-username%3E.github.io/%3Cyour-repo-name%3E/static/js/runtime~main.2e3637c2.js"
+  }
+]);
